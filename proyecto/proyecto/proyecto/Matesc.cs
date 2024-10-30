22507103f93ef6bb4a62816b289a2f6b0173cd63
jemi1419 <camj081024hgtlrna2@cbtis217.edu.mx>
2024-10-30 08:01:31 -0600

    Matematicas

---
+++ proyecto/proyecto/proyecto/Matesc.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: CC2_PC11
+ * Date: 29/10/2024
+ * Time: 07:28 a. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace proyecto
+{
+	/// <summary>
+	/// Description of Matesc.
+	/// </summary>
+	public partial class Matesc : Form
+	{
+		public Matesc()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
