32056dcb5d67949e55e45732af38a022aad2a92f
arwgrvs <goal080813hgtrgna2@cbtis217.edu.mx>
2024-10-30 14:56:05 +0100

    Fisica

---
+++ proyecto/proyecto/Fisica.Designer.cs
@@ -0,0 +1,181 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: CC2_PC43
+ * Date: 24/10/2024
+ * Time: 04:23 p. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace proyecto
+{
+	partial class Fisica
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Fisica));
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.label4 = new System.Windows.Forms.Label();
+			this.pictureBox2 = new System.Windows.Forms.PictureBox();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(23, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(231, 62);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Primera Ley de Newton";
+			// 
+			// label2
+			// 
+			this.label2.Location = new System.Drawing.Point(12, 104);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(242, 72);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "La primera ley postula que un cuerpo permanecerá en reposo o en movimiento recto " +
+			"con una velocidad constante, a menos que se aplique una fuerza externa.";
+			this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// label3
+			// 
+			this.label3.Location = new System.Drawing.Point(12, 167);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(242, 73);
+			this.label3.TabIndex = 2;
+			this.label3.Text = "No es posible que un cuerpo cambie su estado inicial (sea de reposo o movimiento)" +
+			" a menos que intervengan una o varias fuerzas.";
+			this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+			this.pictureBox1.Location = new System.Drawing.Point(66, 306);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(144, 51);
+			this.pictureBox1.TabIndex = 3;
+			this.pictureBox1.TabStop = false;
+			// 
+			// label4
+			// 
+			this.label4.Location = new System.Drawing.Point(111, 284);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(48, 19);
+			this.label4.TabIndex = 4;
+			this.label4.Text = "Fórmula";
+			// 
+			// pictureBox2
+			// 
+			this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+			this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+			this.pictureBox2.Location = new System.Drawing.Point(271, 81);
+			this.pictureBox2.Name = "pictureBox2";
+			this.pictureBox2.Size = new System.Drawing.Size(292, 170);
+			this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox2.TabIndex = 5;
+			this.pictureBox2.TabStop = false;
+			// 
+			// label5
+			// 
+			this.label5.Location = new System.Drawing.Point(271, 294);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(154, 24);
+			this.label5.TabIndex = 6;
+			this.label5.Text = "Si las Fuerzas (Σ F) es igual a cero";
+			// 
+			// label6
+			// 
+			this.label6.Location = new System.Drawing.Point(271, 318);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(189, 39);
+			this.label6.TabIndex = 7;
+			this.label6.Text = "Entonces el cambio en su velocidad con respecto al tiempo (dv / dt), también será" +
+			" igual a cero.";
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(488, 385);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 8;
+			this.button1.Text = ">";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(12, 385);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 9;
+			this.button2.Text = "<";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.Button2Click);
+			// 
+			// Fisica
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(575, 420);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.pictureBox2);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Name = "Fisica";
+			this.Text = "Fisica";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+			this.ResumeLayout(false);
+		}
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.PictureBox pictureBox2;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label1;
+	}
+}
